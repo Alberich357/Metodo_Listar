f97d681cf2fbe7c0bac3cf8378221dd25b7ec80b
Alberich357 <allerenas4@ucol.mx>
2021-10-05 23:31:55 -0500

    grupo

---
+++ grupo.js
@@ -0,0 +1,15 @@
+export default class Grupo{
+constructor(){
+        this.inicio=null;
+    }
+    agregar(nuevo){
+        if (this.inicio==null){
+            this.inicio=nuevo;
+        } else {
+            let aux=this.inicio;
+            while(aux.siguiente!=null)
+                aux=aux.siguiente;
+            aux.siguiente=nuevo;
+        }
+    }
+}
