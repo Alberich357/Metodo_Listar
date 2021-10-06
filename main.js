623ba5a5bf0cbcbc679a0ba1a71558dbb7c55b26
Alberich357 <allerenas4@ucol.mx>
2021-10-05 23:50:07 -0500

    main

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-import Grupo from "./grupo";
+//borré clase grupo
 class Persona{
-    constructor(nombre, edad){
-        this._nombre = nombre;
-        this._edad = edad;
-        this._siguiente = null;
+    constructor(nombre,edad){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.siguiente=null;
     }
-
-
 }
-new Persona; 
+
+
+class Grupo{
+    constructor(){
+        this.inicio=null;
+    }
+    agregar(nuevo){
+        if (this.inicio==null){
+            this.inicio=nuevo;
+        } else {
+            let aux=this.inicio;
+            while(aux.siguiente!=null)
+                aux=aux.siguiente;
+            aux.siguiente=nuevo;
+        }
+    }
+    listar(){
+        if(this.inicio == null){
+            return null;
+        }
+        else {
+            let aux=this.inicio
+            let lista = ""
+            while(aux!=null){
+                lista += 'Nombre:${aux.getName()}, Edad:${aux.getAge()}'
+                aux=aux.siguiente;
+            }
+            return lista
+        }
+    }
+}
