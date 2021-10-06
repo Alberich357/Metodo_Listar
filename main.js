f5f1c9be372e0fc831ba15065c6355c81d215034
Alberich357 <allerenas4@ucol.mx>
2021-10-05 23:31:39 -0500

    main

---
+++ main.js
@@ -0,0 +1,11 @@
+import Grupo from "./grupo";
+class Persona{
+    constructor(nombre, edad){
+        this._nombre = nombre;
+        this._edad = edad;
+        this._siguiente = null;
+    }
+
+
+}
+new Persona; 
